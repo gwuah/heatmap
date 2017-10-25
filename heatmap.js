a3b9e8f19679d5e04d8111944862ec6c7a6dfd45
Griffith Awuah <griffith@dolphtech.com>
2017-10-25 21:20:10 +0100

    added map drawing feature and also initiated a local session to spare the dev the hassle of running the scipt. Just load and click like your head no dey!

--- heatmap.js
+++ heatmap.js
@@ -1,4 +1,5 @@
 (function(global, local) {
+	'use strict';
 
 	// necessary variables
 	let hm;
@@ -30,6 +31,26 @@
 
 				hm.count += 1;
 			})
+		},
+
+		drawHeatMap: function() {
+			for (const coord of hm.database) {
+				// plot div on body
+				local.body.appendChild(hm.createDiv(coord));
+			}
+		},
+
+		createDiv: function(coord) {
+			let div = document.createElement("div");
+			div.style.backgroundColor = "black";
+			div.style.height = "5px";
+			div.style.width = "5px";
+			div.style.borderRadius = "5px";
+			div.style.position = "absolute";
+			div.style.left = `${coord.x}px`
+			div.style.top = `${coord.y}px`
+
+			return div
 		}
 	}
 
@@ -39,6 +60,24 @@
 	// expose api to window
 	global.hm = heatMap;
 
-	// console.log(this)
+	let session = heatMap();
+	session.detectActivity();
+
+	// on click on ctrl key, draw heatmap
+	local.addEventListener("keydown", e=> {
+	    // console.log(e);
+	    if (e.keyCode == 17) {
+	    	console.log("drawing heatmap on page");
+	    	session.drawHeatMap();
+	    } else {
+	    	console.log("other!")
+	    }
+	})
+
+	global.session = session;
 
 })(window, document)
+
+// to use; just load the javascript file into your page
+// click randomly
+// press on control to see your graph
